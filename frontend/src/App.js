7a9143402afa55c9f3f7a9bdb5a284c4fa912ebd
Shreyas <shreyaskumar0411@gmail.com>
2024-11-07 23:21:53 +0530

    added nav hero footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
-   <>
-    
-   </>
+    <Router>
+      <div className="container mx-auto px-8">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
   );
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-gunmetal": "#0C2B36",
+        "off-white": "#FEFEFE",
+        "mint-cream": "#C2E5D3",
+        "pure-white": "#FFFFFF",
+        "deep-mint": "#50B682",
+      },
+      backgroundImage: {
+        'gradient-top': "linear-gradient(0deg, #0C2B36, #FEFEFE, #C2E5D3, #FFFFFF, #50B682)",
+        'gradient-right': "linear-gradient(90deg, #0C2B36, #FEFEFE, #C2E5D3, #FFFFFF, #50B682)",
+        'gradient-bottom': "linear-gradient(180deg, #0C2B36, #FEFEFE, #C2E5D3, #FFFFFF, #50B682)",
+        'gradient-left': "linear-gradient(270deg, #0C2B36, #FEFEFE, #C2E5D3, #FFFFFF, #50B682)",
+        'gradient-top-right': "linear-gradient(45deg, #0C2B36, #FEFEFE, #C2E5D3, #FFFFFF, #50B682)",
+        'gradient-bottom-right': "linear-gradient(135deg, #0C2B36, #FEFEFE, #C2E5D3, #FFFFFF, #50B682)",
+        'gradient-top-left': "linear-gradient(225deg, #0C2B36, #FEFEFE, #C2E5D3, #FFFFFF, #50B682)",
+        'gradient-bottom-left': "linear-gradient(315deg, #0C2B36, #FEFEFE, #C2E5D3, #FFFFFF, #50B682)",
+        'gradient-radial': "radial-gradient(#0C2B36, #FEFEFE, #C2E5D3, #FFFFFF, #50B682)",
+      }
+    },
   },
   plugins: [],
 }
